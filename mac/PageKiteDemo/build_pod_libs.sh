4722f85f5dba1585c764f94108d00afa173a5c74
Jay Lyerly <jlyerly@stepleaderdigital.com>
2014-06-20 14:32:22 -0400

    More debug tweaks.

--- mac/PageKiteDemo/build_pod_libs.sh
+++ mac/PageKiteDemo/build_pod_libs.sh
@@ -10,11 +10,18 @@ WORKSPACE=PageKiteDemo.xcworkspace
 
 pod install
 
+echo CLEANING
 xcodebuild -workspace $WORKSPACE -scheme libev -configuration $CONFIGURATION clean
 xcodebuild -workspace $WORKSPACE -scheme PageKiteKit -configuration $CONFIGURATION clean
 xcodebuild -workspace $WORKSPACE -scheme PageKiteKitIOS -configuration $CONFIGURATION clean
 
+echo BUILDING LIBEV
 xcodebuild -workspace $WORKSPACE -scheme libev -configuration $CONFIGURATION
+
+echo LIBEV STATUS
+ls -la ../ThirdParty/libev-4.15/lib*
+
+echo BUILDING MAC PAGEKITEKIT
 
 MAC_PRODUCTS_DIR=`xcodebuild -workspace $WORKSPACE -scheme PageKiteKit -configuration $CONFIGURATION -showBuildSettings|grep ' BUILT_PRODUCTS_DIR ='|head -1|awk -F= '{print $2}'|tr -d ' '`
 xcodebuild -workspace $WORKSPACE -scheme PageKiteKit -configuration $CONFIGURATION 
@@ -23,6 +30,8 @@ cp -a $MAC_PRODUCTS_DIR/PageKiteKit.framework osx/Frameworks
 # stub out the framework for iOS
 cp -a $MAC_PRODUCTS_DIR/PageKiteKit.framework ios/Frameworks
 rm ios/Frameworks/PageKiteKit.framework/Versions/Current/PageKiteKit
+
+echo BUILDING IOS PAGEKITEKIT
 
 IOS_PRODUCTS_DIR=`xcodebuild -workspace $WORKSPACE -scheme PageKiteKitIOS -configuration $CONFIGURATION -showBuildSettings|grep ' BUILT_PRODUCTS_DIR ='|head -1|awk -F= '{print $2}'|tr -d ' '`
 xcodebuild -workspace $WORKSPACE -scheme PageKiteKitIOS -configuration $CONFIGURATION 
