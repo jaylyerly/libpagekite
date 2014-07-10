ee365453ff8bc7af3c7651f926c7d34c825eefd7
Jay Lyerly <jlyerly@stepleaderdigital.com>
2014-07-10 12:34:31 -0400

    Switch from CIColorClamp filter to CIColorMonochrome filter to blacken the status bar icon. CIColorMonochrome is available all the way back to 10.4 but CIColorClamp is only available on 10.9+.

--- mac/PageKiteApp/PageKite/PageKite/NSImage+ColorMap.m
+++ mac/PageKiteApp/PageKite/PageKite/NSImage+ColorMap.m
@@ -11,67 +11,23 @@
 
 @implementation NSImage (ColorMap)
 
-/**
- *  Take an image and clamp all colors to the given color, except the alpha channel.
- *  For an icon with transparency, it makes all the visible colors the same, leaving all
- *  the transparent areas unaffected.
- *
- *  @return New UIImage with the whitened image
- */
 - (NSImage *)clampToColor:(NSColor *)theColor{
     
-    CGSize size = self.size;
-    
-    NSColorSpaceModel cSpace = theColor.colorSpace.colorSpaceModel;
+    CIColor *coreColor = [CIColor colorWithCGColor:theColor.CGColor];
 
-    // For unknown color space, don't clamp anything.
-    CIVector *colorMin = [[CIVector alloc] initWithX:0.0f
-                                                   Y:0.0f
-                                                   Z:0.0f
-                                                   W:0.0f];
-    CIVector *colorMax = [[CIVector alloc] initWithX:1.0f
-                                                   Y:1.0f
-                                                   Z:1.0f
-                                                   W:1.0f];
-    
-    // extract the color correctly, depending on colorspace
-    switch (cSpace) {
-        case NSGrayColorSpaceModel:
-            colorMin = [[CIVector alloc] initWithX:theColor.whiteComponent
-                                                 Y:theColor.whiteComponent
-                                                 Z:theColor.whiteComponent
-                                                 W:0.0f];
-            colorMax = [[CIVector alloc] initWithX:theColor.whiteComponent
-                                                 Y:theColor.whiteComponent
-                                                 Z:theColor.whiteComponent
-                                                 W:1.0f];
-            break;
-        case NSRGBColorSpaceModel:
-            colorMin = [[CIVector alloc] initWithX:theColor.redComponent
-                                                 Y:theColor.greenComponent
-                                                 Z:theColor.blueComponent
-                                                 W:0.0f];
-            colorMax = [[CIVector alloc] initWithX:theColor.redComponent
-                                                 Y:theColor.greenComponent
-                                                 Z:theColor.blueComponent
-                                                 W:1.0f];
-            break;
-
-        default:
-            break;
-    }
-    
     CIContext *context = [CIContext contextWithCGContext:[[NSGraphicsContext currentContext] graphicsPort]
                                                  options:nil];
     CIImage *image = [CIImage imageWithData:[self TIFFRepresentation]];
-    CIFilter *filter = [CIFilter filterWithName:@"CIColorClamp"];
+    
+    CIFilter *filter = [CIFilter filterWithName:@"CIColorMonochrome"];
     [filter setValue:image forKey:kCIInputImageKey];
-    [filter setValue:colorMin forKey:@"inputMinComponents"];
-    [filter setValue:colorMax forKey:@"inputMaxComponents"];
+    [filter setValue:coreColor forKey:kCIInputColorKey];
+    [filter setValue:@1.0 forKey:@"inputIntensity"];
     CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    
     CGRect extent = [result extent];
     CGImageRef cgImage = [context createCGImage:result fromRect:extent];
-    return [[NSImage alloc] initWithCGImage:cgImage size:size];
+    return [[NSImage alloc] initWithCGImage:cgImage size:self.size];
 }
 
 -(NSImage *)whiten {
